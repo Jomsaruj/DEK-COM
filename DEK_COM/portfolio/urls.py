852c19121bb6383110e81699f36ad9f2e3c5357c
saruj <jomsaruj@sarujs-MacBook-Pro.local>
2020-10-15 01:43:44 +0700

    Add error/success message after logIn and changed TimeZone

--- DEK_COM/portfolio/urls.py
+++ DEK_COM/portfolio/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
     path('login/signin/', views.signin, name='signin'),
+    # path('signout/', views.signout, name='signout'),
 ]
 
 
--- DEK_COM/portfolio/views.py
+++ DEK_COM/portfolio/views.py
@@ -1,27 +1,46 @@
+# from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
+# from django.contrib.auth import logout
+# from django.urls import reverse
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 def index(request):
+    """Redirect user to DEK-COM home page"""
     return render(request, 'portfolio/home.html')
 
 
 def login(request):
+    """Redirect user to DEK-COM log in page"""
     return render(request, 'portfolio/login.html')
     
     
 def signup(request):
+    """Redirect user to DEK-COM sign up page"""
     return render(request, 'portfolio/signup.html')
 
 
 def signin(request):
+    """Get username and password form login.html and then log user in if valid"""
     username = request.GET.get('username')
     password = request.GET.get('password')
     user = authenticate(username=username, password=password)
     if user is not None:
         auth_login(request, user)
+        # This part is only for iteration1
+        messages.success(request, "Hello! " + username + " You are now Log In!")
+        ##################################
         return render(request, 'portfolio/home.html')
     else:
-        return render(request, 'portfolio/signup.html')
+        messages.error(request, "Username or Password is incorrect")
+        return redirect('portfolio:login')
+
+# NOTE: not finish
+# def signout(request):
+#     """log user out"""
+#     logout(request)
+#     return HttpResponseRedirect(reverse('portfolio:index'))
 
