a3812d16808f098e4839a742c9ed51f2d760ef8c
Patiphan Srichai <patiphansrichai@Patiphans-MacBook-Pro.local>
2020-10-15 23:35:30 +0700

    Implement bootstrap to improve registration and login page appearance

--- DEK_COM/DEK_COM/urls.py
+++ DEK_COM/DEK_COM/urls.py
@@ -22,6 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('', include('portfolio.urls')),
-    # path('login/', include("django.contrib.auth.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
--- DEK_COM/portfolio/urls.py
+++ DEK_COM/portfolio/urls.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    # path('login/', views.login, name='login')
+    path('', views.index, name = 'index'),
 ]
