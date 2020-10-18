9befc034144ada45eec33b43364edbb3a1cc512b
Patiphan Srichai <patiphansrichai@Patiphans-MacBook-Pro.local>
2020-10-19 00:06:06 +0700

    Add logout page

--- DEK_COM/DEK_COM/urls.py
+++ DEK_COM/DEK_COM/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('register/', users_views.register, name='register'),
     path('', include('portfolio.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('login/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
