7edfd836319d306b2ebc2ff1a030f6bd7c9542e7
Patiphan Srichai <patiphansrichai@Patiphans-MacBook-Pro.local>
2020-10-18 14:24:59 +0700

    Improve alert message and delete duplicate registration file

--- DEK_COM/users/views.py
+++ DEK_COM/users/views.py
@@ -8,7 +8,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():  # all information is valid
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome to DEK-COM {username}!!')
+            messages.success(request, f'Welcome to DEK-COM {username} !!')
             return redirect('index')  # redirect to portfolio index (home.html)
     else:
         form = UserCreationForm()
