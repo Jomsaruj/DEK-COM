249a8a1835fbee1cf5b3a92d5d4e223198ad1c51
Patiphan Srichai <patiphansrichai@Patiphans-MacBook-Pro.local>
2020-10-18 14:14:04 +0700

    Create registration page

--- DEK_COM/DEK_COM/urls.py
+++ DEK_COM/DEK_COM/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
-from users import views as user_views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
+    path('register/', users_views.register, name='register'),
     path('', include('portfolio.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
--- DEK_COM/users/views.py
+++ DEK_COM/users/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.views import View
 
 def register(request):
     if request.method == 'POST':  # user click SignUp button
-        print("submit")
         form = UserCreationForm(request.POST)
         if form.is_valid():  # all information is valid
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome to DEK-COM {username}!!')
-            return redirect('/home')  # redirect to portfolio index (home.html)
+            return redirect('index')  # redirect to portfolio index (home.html)
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
