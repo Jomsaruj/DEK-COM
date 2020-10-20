658065a454a17fe6c54a8485ec561d0fef98a853
AuttaKrit Wongsarawit <auttakrit.w@ku.th>
2020-10-20 21:41:29 +0700

    create post and comment class

--- DEK_COM/blog/models.py
+++ DEK_COM/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    post_topic = models.CharField(max_length=100)
+    post_text = models.CharField(max_length=200)
+
+    def modify_post(self, new_text: str):
+        self.post_text = new_text
+
+    def __str__(self):
+        return self.post_text
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.CharField(max_length=200)
+
+    def modify_comment(self, new_text: str):
+        self.comment_text = new_text
+
+    def __str__(self):
+        return self.comment_text
