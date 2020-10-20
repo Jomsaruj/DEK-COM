43350251ade84a7699b4fb5c725a6798381c5465
AuttaKrit Wongsarawit <auttakrit.w@ku.th>
2020-10-20 21:52:22 +0700

    add like in Post and Comment

--- DEK_COM/blog/models.py
+++ DEK_COM/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     post_topic = models.CharField(max_length=100)
     post_text = models.CharField(max_length=200)
+    post_like = models.IntegerField(default=0)
 
     def modify_post(self, new_text: str):
         self.post_text = new_text
@@ -15,6 +16,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=200)
+    comment_like = models.IntegerField(default=0)
 
     def modify_comment(self, new_text: str):
         self.comment_text = new_text
