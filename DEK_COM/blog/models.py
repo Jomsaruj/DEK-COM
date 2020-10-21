4bff6b0ed90a0e7210447c2c50b3c82e7c4f4a30
AuttaKrit Wongsarawit <auttakrit.w@ku.th>
2020-10-21 09:32:07 +0700

    create set and get in Post and Comemnt Class

--- DEK_COM/blog/models.py
+++ DEK_COM/blog/models.py
@@ -6,8 +6,23 @@ class Post(models.Model):
     post_text = models.CharField(max_length=200)
     post_like = models.IntegerField(default=0)
 
-    def modify_post(self, new_text: str):
+    def set_like(self, new_like: str):
+        self.post_like = new_like
+
+    def set_text(self, new_text: str):
         self.post_text = new_text
+
+    def set_topic(self, new_topic: str):
+        self.post_topic = new_topic
+
+    def get_like(self):
+        return self.post_like
+
+    def get_text(self):
+        return self.post_text
+
+    def get_topic(self):
+        return self.post_topic
 
     def __str__(self):
         return self.post_text
@@ -18,8 +33,23 @@ class Comment(models.Model):
     comment_text = models.CharField(max_length=200)
     comment_like = models.IntegerField(default=0)
 
-    def modify_comment(self, new_text: str):
+    def set_like(self, new_like: str):
+        self.comment_like = new_like
+
+    def set_text(self, new_text: str):
         self.comment_text = new_text
 
+    def set_topic(self, new_topic: str):
+        self.comment_topic = new_topic
+
+    def get_like(self):
+        return self.comment_like
+
+    def get_text(self):
+        return self.comment_text
+
+    def get_topic(self):
+        return self.comment_topic
+    
     def __str__(self):
         return self.comment_text
