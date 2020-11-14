bf78f422d09622f1602f2ca8de84dc72eb08dbd3
AuttaKrit Wongsarawit <auttakrit.w@ku.th>
2020-11-14 17:59:21 +0700

    create class coin in users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Coin(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_coin = models.IntegerField(default=0)
+
+    def get_total_coin(self):
+        return self.total_coin
+
+    def __str__(self):
+        return f"{self.total_coin}"
