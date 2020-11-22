096bfd66a6f1bc9f42dd34a3854d0080b4e77527
Ing140943 <skladee@hotmail.co.th>
2020-11-23 00:53:05 +0700

    Add search features in progressing

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from blog.models.post import Post
+from django.db.models import Q 
 
 
 def index(request):
-    return render(request, 'portfolio/home.html')
-
+    search_post = request.GET.get('search')
+    if search_post:
+        posts = Post.objects.filter(Q(title__icontains=search_post) or Q(content__icontains=search_post))
+    return render(request, 'portfolio/profile.html', {'posts': posts})
 
 def profile(request, username):
     user_profile = User.objects.filter(username=username).first()
