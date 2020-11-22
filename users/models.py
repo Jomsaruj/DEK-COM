68f9774dc94858795e1b363469b4c6f173f10153
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2020-11-22 13:21:37 +0700

    add user tags

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Coin(models.Model):
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     total_coin = models.IntegerField(default=0)
 
@@ -15,3 +16,38 @@ class Coin(models.Model):
 
     def __str__(self):
         return f"{self.total_coin}"
+
+
+class Profession(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_java = models.BooleanField(default=False)
+    is_python = models.BooleanField(default=False)
+    is_JavaScript = models.BooleanField(default=False)
+    is_Csharp = models.BooleanField(default=False)
+    is_C = models.BooleanField(default=False)
+    is_Cpp = models.BooleanField(default=False)
+    is_Go = models.BooleanField(default=False)
+    is_R = models.BooleanField(default=False)
+    is_Swift = models.BooleanField(default=False)
+    is_PHP = models.BooleanField(default=False)
+    is_Kotlin = models.BooleanField(default=False)
+    is_Ruby = models.BooleanField(default=False)
+    is_Dart = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profession.objects.create(user=instance)
+    instance.profession.save()
+
+
+
+
+
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from users.form import CustomRegistrationForm
-from django.views import View
+
 
 def register(request):
     if request.method == 'POST':  # user click SignUp button
         form = CustomRegistrationForm(request.POST)
         if form.is_valid():  # all information is valid
-            form.save()
+            user = form.save()
+            user.refresh_from_db()
+            user.profession.is_java = form.cleaned_data.get('is_java')
+            user.profession.is_python = form.cleaned_data.get('is_python')
+            user.profession.is_JavaScript = form.cleaned_data.get('is_JavaScript')
+            user.profession.is_Csharp = form.cleaned_data.get('is_Csharp')
+            user.profession.is_C = form.cleaned_data.get('is_C')
+            user.profession.is_Cpp = form.cleaned_data.get('is_Cpp')
+            user.profession.is_Go = form.cleaned_data.get('is_Go')
+            user.profession.is_R = form.cleaned_data.get('is_R')
+            user.profession.is_Swift = form.cleaned_data.get('is_Swift')
+            user.profession.is_PHP = form.cleaned_data.get('is_PHP')
+            user.profession.is_Kotlin = form.cleaned_data.get('is_Kotlin')
+            user.profession.is_Ruby = form.cleaned_data.get('is_Ruby')
+            user.profession.is_Dart = form.cleaned_data.get('is_Dart')
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome to DEK-COM {username} !!')
+            user.save()
+            messages.success(request, f'Welcome to DEK-COM {username}!!')
             return redirect('login')  # redirect to portfolio index (home.html)
     else:
         form = CustomRegistrationForm()
-    return render(request, 'users/register.html', {'form': form})
 
+    return render(request, 'users/register.html', {'form': form})
