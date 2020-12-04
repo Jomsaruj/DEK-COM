c51538133599fb0f903489e52b062b16e2393acb
patiphan2000 <patiphan.sri@ku.th>
2020-12-04 03:35:54 +0000

    Initial commit on cloud branch

--- DEK_COM/wsgi.py
+++ DEK_COM/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DEK_COM.settings')
 
 application = get_wsgi_application()
+
+sys.path.append('/var/www/project/backend')
