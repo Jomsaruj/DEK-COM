dfd592548c44064b2f870594b585c2e8b428958a
patiphan2000 <patiphan.sri@ku.th>
2020-12-16 10:52:08 +0700

    tag filter by url dispatch

--- blog/views/blog_views.py
+++ blog/views/blog_views.py
@@ -16,13 +16,18 @@ from .job_views import *
 def blog(request):
     all_tag = Tag.objects.all().order_by('-post_num')[:8]
     search_post = request.GET.get('search')
+
+    tag_filter = request.GET.getlist('tag')
+
     if search_post:
         most_recent_post = Post.objects.filter(Q(topic__icontains=search_post) | Q(content__icontains=search_post))
         return render(request, 'blog/blog_index.html', {'most_recent_post': most_recent_post, 'popular_tag': all_tag})
     for tag in all_tag:
         TagManager.update_tag_num(tag.name)
-    selected_tags = Tag.objects.filter(active_status=True)
-    if Tag.objects.filter(active_status=True).count() > 0:
+    
+    selected_tags = Tag.objects.filter(name__in=tag_filter)
+    
+    if selected_tags.count() > 0:
         most_recent_post = Blog.objects.filter(
             Q(Post___tags__in=selected_tags) | 
             Q(Question___tags__in=selected_tags) | 
