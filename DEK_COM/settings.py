48d32569fc932f2c6e6bd86f4e5d55dae2859cee
patiphan2000 <patiphan.sri@ku.th>
2021-11-22 21:05:27 +0700

    fix allow host in setting

--- DEK_COM/settings.py
+++ DEK_COM/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://dek-com.herokuapp.com']
+ALLOWED_HOSTS = ['dek-com.herokuapp.com']
 
 # Application definition
 
